bdb7fe778e673fcbad48239a11b644a0bb134e8d
0x676e67 <gngppz@gmail.com>
2025-09-24 17:32:11 +0800

    feat: make log parameter global across all commands (#177)

--- src/main.rs
+++ src/main.rs
@@ -126,7 +126,7 @@ pub enum Proxy {
 #[derive(Args, Clone)]
 pub struct BootArgs {
     /// Log level e.g. trace, debug, info, warn, error
-    #[clap(long, env = "VPROXY_LOG", default_value = "info")]
+    #[clap(long, env = "VPROXY_LOG", default_value = "info", global = true)]
     log: tracing::Level,
 
     /// Bind address
